d3bd2b5654d4f1c179d7e7a1516da4e900d5aa63
yusuke-morishita <106517058+yusuke-morishita@users.noreply.github.com>
2024-06-11 02:58:09 +0900

    Update test_smile_model_lenet1.py

--- 2024/test_smile_model_lenet1.py
+++ 2024/test_smile_model_lenet1.py
@@ -43,51 +43,42 @@ model.load_state_dict(torch.load('model_weights.pth'))
 # Initialize a face detector by OpenCV
 detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Initialize a camera capture
-cam = cv2.VideoCapture(0)
 
 model.eval()
 with torch.no_grad():
-    while True:
-        # Capture one frame from camera
-        ret, frame = cam.read()
-        img_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Capture one image from camera
+    filename = take_photo()
+    frame = cv2.imread(filename)
+    img_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces
-        faces = detector.detectMultiScale(img_g)
-        for x, y, w, h in faces:
-            # Show detection results
-            #print(x, y, w, h)
+    # Detect faces
+    faces = detector.detectMultiScale(img_g)
+    for x, y, w, h in faces:
+        # Show detection results
+        #print(x, y, w, h)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # Create a norm image
+        img_norm = cv2.resize(img_g[y:y + h, x:x + w], dsize = (32, 32))
+
+        # Convert into pytorch format
+        inputs = torch.from_numpy(img_norm.reshape((1, 1, 32, 32)).astype(np.float32))
+        inputs = inputs.to(device)
+
+        # Inference
+        outputs = model(inputs)
+
+        # Retrieve an inference result
+        pred = outputs.argmax(1)
+        print('Softmax: {0:.3f}'.format(np.exp(outputs[0, 1].item()) /
+            (np.exp(outputs[0, 0].item()) + np.exp(outputs[0, 1].item()))))
+        print('Result: {} => {}'.format(pred[0].item(), 'smile' if pred[0] > 0.5 else 'neutral'))
+
+        # Show detection results
+        if pred[0] > 0.5:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        else:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-            # Create a norm image
-            img_norm = cv2.resize(img_g[y:y + h, x:x + w], dsize = (32, 32))
-
-            # Convert into pytorch format
-            inputs = torch.from_numpy(img_norm.reshape((1, 1, 32, 32)).astype(np.float32))
-            inputs = inputs.to(device)
-
-            # Inference
-            outputs = model(inputs)
-
-            # Retrieve an inference result
-            pred = outputs.argmax(1)
-            print('Softmax: {0:.3f}'.format(np.exp(outputs[0, 1].item()) /
-                (np.exp(outputs[0, 0].item()) + np.exp(outputs[0, 1].item()))))
-            print('Result: {} => {}'.format(pred[0].item(), 'smile' if pred[0] > 0.5 else 'neutral'))
-
-            # Show detection results
-            if pred[0] > 0.5:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            else:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Show image
-        cv2.imshow('image', frame)
-        key = cv2.waitKey(10)
-        if key == 27 or key == 'q': # ESC or q
-            break
-
-    cam.release()
-    cv2.destroyAllWindows()
-
+    # Show image
+    cv2_imshow(frame)
